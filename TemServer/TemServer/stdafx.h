03fd8ce805d64343a1020a80749250d5673751a7
chalbangE <yubin41029@naver.com>
2024-11-08 01:05:26 +0900

    include용 stdafx.h 생성

---
+++ TemServer/TemServer/stdafx.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <array>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <algorithm>
+#include <unordered_set>
+#include <concurrent_priority_queue.h>
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+#include <windows.h>  
+
+#include "protocol.h"
+	// + enum.h
